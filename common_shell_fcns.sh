0fae502c593206750c381be94517a29c34748c7c
Werner <Werner Freund>
2016-05-04 01:06:15 -0300

    Protected from exporting IFS changes to other processes

--- common_shell_fcns.sh
+++ common_shell_fcns.sh
@@ -1,16 +1,16 @@
 find_lib()
 {
   array=$(echo $LD_LIBRARY_PATH | tr ':' ' ' )
-  OLD_IFS=$IFS; export IFS=' ';
+  OLD_IFS=$IFS; IFS=' ';
   for place in $array; do
     #echo "Searching on $place" >&2 
     if test -n "$(find $place -mindepth 1 -maxdepth 1 -name "$1*" 2>/dev/null)"; then
-      export IFS=$OLD_IFS
+      IFS=$OLD_IFS
       echo "${place}"
       return 0;
     fi
   done
-  export IFS=$OLD_IFS
+  IFS=$OLD_IFS
   return 1;
 }
 
